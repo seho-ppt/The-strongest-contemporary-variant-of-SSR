dc4c7d39732a03ee0240e815dee1084b6a0231bd
seho <1018715564@qq.com>
2022-09-10 23:47:09 +0800

    ✨ feat: 框架代码演示

---
+++ components/RenderWindow.vue
@@ -0,0 +1,9 @@
+<script>
+</script>
+<template>
+  <div class="relative">
+    <img class="absolute top-0 left-35" width="600" height="80" src="https://static.yinzhuoei.com/typecho/2022/09/10/00377784728673/WX20220910-165918@2x.png"/>
+  </div>
+</template>
+<style scoped>
+</style>
